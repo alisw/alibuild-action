18a1d88d45d10dcde30a1592a5c445ad1f1b6aaf
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-12-17 00:46:57 +0100

    Allow for extra packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,12 @@ PACKAGE=$1
 ALIBUILD_SLUG=$2
 ALIDIST_SLUG=$3
 DEFAULTS=$4
+EXTRA_PACKAGES=$5
+
+yum install $EXTRA_PACKAGES
 
 git clone https://github.com/alisw/alibuild
 git clone https://github.com/alisw/alidist
 
+alibuild/aliBuild doctor --defaults o2 $PACKAGE --debug
 alibuild/aliBuild build --defaults o2 $PACKAGE
