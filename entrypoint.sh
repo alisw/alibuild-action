a63cdecb3136ef1f5a5e6ed58b083a9b272de935
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2023-01-12 11:34:39 +0100

    Add debug information

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,11 +14,15 @@ if [ ! "X$REMOVE_PACKAGES" = X ]; then
   yum remove -y $REMOVE_PACKAGES
 fi
 
+echo Checking out code in $PWD
 cd ..
 git clone https://github.com/alisw/alibuild
 git clone https://github.com/alisw/alidist
 
+ls -la
+
 pip3 install -e alibuild
 
+echo Building in in $PWD
 aliBuild doctor --defaults o2 $PACKAGE --debug
 aliBuild build --defaults o2 $PACKAGE
